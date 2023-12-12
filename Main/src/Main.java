f028cee253b05344dd91b38480cd760bc393d177
hwjw20 <hwjw666@naver.com>
2023-12-12 19:05:49 +0900

    [황지원] - 최초 파일 추가

---
+++ Main/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class Main {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		String[][] answer = {};
+		HashMap<Integer, String> map = new HashMap<>();
+		map.put(1, "hi");
+		map.put(0, "Hello");
+		map.put(1, "i want to go home");
+		
+		int max = 0;
+		for(int i = 0; i < map.size(); i++) {
+			
+			map.get(max);
+			System.out.println(map.get(max));
+			
+		}
+	}
+
+}
